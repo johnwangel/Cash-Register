1c1430d798fcb8b7bbf9ed44ebb34449f0c838e9
John Atkins <johnatkins1999@yahoo.com>
2017-06-15 20:07:10 -1000

    no math

--- calculator.js
+++ calculator.js
@@ -3,40 +3,104 @@
       let module = {};
 
       module.changeDisplay = function(id){
+          if (window.operators.getOperand() !==""){ window.operators.clearDisplay(); }
           let currentString = document.getElementById("display").innerText;
           if (currentString === "0") { currentString = ""; }
           let myString = currentString + id.toString();
           document.getElementById("display").innerText = myString;
-          return Number(myString);
-      }
 
+      }
 
       return module;
   })();
 
   window.operators = (function(){
 
+      let operand = "";
+      let tempMemoryValue = 0;
+      let displayNumber = 0;
+
       let module = {};
+
+      module.changeOperand = function(y){
+        operand = y;
+      }
+
+      module.storeTempMemVal = function(num){
+        tempMemoryValue = num;
+      }
+
+      module.getTempMemVal = function(num){
+        return tempMemoryValue;
+      }
+
+      module.storeDisplayNumber = function(n){
+        displayNumber  = n;
+      }
+
+      module.getDisplayNumber = function(){
+        return displayNumber;
+      }
+
+      module.getOperand = function(){
+        return operand;
+      }
 
       module.clearDisplay = function(){
           document.getElementById("display").innerText = 0;
       }
 
-
       module.getBalance = function(){
           let myString =  window.calculator.getTotal().toString();
-          alert (myString);
-          document.getElementById("display").innerText = myString;
+          alert("Current balance: " + myString);
+          //document.getElementById("display").innerText);
       }
 
       module.depositCash = function(){
-          let myDeposit =  Number(document.getElementById("display").innerText);
+          let myDeposit =  Number(document.getElementById("display").innerText) + window.calculator.getTotal();
+          window.calculator.load(myDeposit);
           window.operators.clearDisplay();
+      }
+
+      module.withdrawCash = function(){
+          let myWithdrawal = window.calculator.getTotal() - Number(document.getElementById("display").innerText);
+          window.calculator.load(myWithdrawal);
+          window.operators.clearDisplay();
+      }
+
+      module.math = function(id){
+        window.operators.storeTempMemVal(Number(document.getElementById("display").innerText));
+        //window.operators.clearDisplay();
+        window.operators.changeOperand(id);
+      }
+
+      module.equals = function(){
+          window.operators.storeDisplayNumber(document.getElementById("display").innerText);
+
+          switch (window.operators.getOperand()) {
+            case "add":
+              let num = 0;
+              num = window.operators.getTempMemVal() + Number(window.operators.getDisplayNumber());
+              console.log(window.operators.getTempMemVal());
+              console.log(Number(window.operators.getDisplayNumber()));
+              console.log(num);
+              console.log(typeof num);
+              document.getElementById("display").innerText = window.operators.getTempMemVal() + Number(window.operators.getDisplayNumber());
+            case "subtract":
+              document.getElementById("display").innerText = window.operators.getTempMemVal() - Number(document.getElementById("display").innerText);
+            case "multiply":
+              document.getElementById("display").innerText = window.operators.getTempMemVal() * Number(document.getElementById("display").innerText);
+            case "divide":
+              document.getElementById("display").innerText = window.operators.getTempMemVal() / Number(document.getElementById("display").innerText);
+            default:
+              document.getElementById("display").innerText = Number(document.getElementById("display").innerText);
+        }
+        window.operators.storeTempMemVal(0);
+        window.operators.changeOperand("");
       }
 
       return module;
   })();
-
 
  window.calculator = (function(){
 
